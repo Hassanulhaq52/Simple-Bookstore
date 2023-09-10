039433d19e21b7d20022477ce823883f304632b8
hassanulhaq52 <hassanulhaq52@gmail.com>
2023-09-11 02:08:48 +0500

    UI Modified and Search Functionality Added.

--- lib/screens/book_details_screen.dart
+++ lib/screens/book_details_screen.dart
@@ -1,3 +1,58 @@
+// import 'package:flutter/material.dart';
+// import 'package:simple_bookstore/screens/cart_screen.dart';
+//
+// import '../model/book_model.dart';
+//
+// class BookDetailsScreen extends StatelessWidget {
+//   final Book book;
+//
+//   const BookDetailsScreen({super.key, required this.book});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(book.title),
+//       ),
+//       body: SingleChildScrollView(
+//         scrollDirection: Axis.vertical,
+//         child: Padding(
+//           padding: const EdgeInsets.all(8.0),
+//           child: Column(
+//
+//             crossAxisAlignment: CrossAxisAlignment.center,
+//             children: [
+//               Image.asset(book.image,
+//               height: 500,
+//               width: 300,),
+//               Text('Title: ${book.title}'),
+//               Text('Author: ${book.author}'),
+//               Text('Price: \$${book.price.toStringAsFixed(2)}'),
+//               Text('Description: ${book.description}'),
+//               ElevatedButton(
+//                 onPressed: () {
+//                   Navigator.push(
+//                     context,
+//                     MaterialPageRoute(
+//                       builder: (context) => const CartScreen(),
+//                     ),
+//                   );
+//                 },
+//                 child: const Text('Add to Cart'),
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
+
+
+
 import 'package:flutter/material.dart';
 import 'package:simple_bookstore/screens/cart_screen.dart';
 
@@ -6,7 +61,7 @@ import '../model/book_model.dart';
 class BookDetailsScreen extends StatelessWidget {
   final Book book;
 
-  const BookDetailsScreen({super.key, required this.book});
+  const BookDetailsScreen({Key? key, required this.book}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,26 +69,91 @@ class BookDetailsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(book.title),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Image.asset(book.image),
-          Text('Title: ${book.title}'),
-          Text('Author: ${book.author}'),
-          Text('Price: \$${book.price.toStringAsFixed(2)}'),
-          Text('Description: ${book.description}'),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const CartScreen(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                height: 400,
+                width: 300,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
                 ),
-              );
-            },
-            child: const Text('Add to Cart'),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset(
+                    book.image,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+              Text(
+                book.title,
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 8),
+              Text(
+                'Author: ${book.author}',
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+              ),
+              SizedBox(height: 8),
+              Text(
+                'Price: \$${book.price.toStringAsFixed(2)}',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Description:',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(height: 8),
+              Text(
+                book.description,
+                style: TextStyle(fontSize: 16),
+              ),
+              SizedBox(height: 24),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const CartScreen(),
+                    ),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue, // Button color
+                  elevation: 3, // Shadow
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                  child: Text(
+                    'Add to Cart',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
--- lib/widgets/book_list.dart
+++ lib/widgets/book_list.dart
@@ -1,29 +1,369 @@
+// import 'package:flutter/material.dart';
+// import 'package:simple_bookstore/screens/book_details_screen.dart';
+// import 'package:simple_bookstore/utils/constants.dart';
+//
+// import '../model/book_model.dart';
+//
+// class BookList extends StatelessWidget {
+//   const BookList({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     final List<Book> books = [
+//       Book('QUEERBOOK', 'Maia Kobabe', 20, Constants.queerBookDescription, 'images/queens.png'),
+//       Book('THE NOTE', 'Nicholas Sparks', 30, Constants.noteBookDescription, 'images/the_note.png'),
+//       Book('THE PATIENT', 'Eric Topol', 25, Constants.patientBookDescription, 'images/the_patient.png'),
+//     ];
+//
+//     return ListView.builder(
+//       itemCount: books.length,
+//       itemBuilder: (context, index) {
+//         final book = books[index];
+//         return ListTile(
+//           leading: Image.asset(book.image),
+//           title: Text(book.title),
+//           subtitle: Text(book.author),
+//           trailing: Text('\$${book.price.toStringAsFixed(2)}'),
+//           onTap: () {
+//             Navigator.push(
+//               context,
+//               MaterialPageRoute(
+//                 builder: (context) => BookDetailsScreen(book: book),
+//               ),
+//             );
+//           },
+//         );
+//       },
+//     );
+//   }
+// }
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:simple_bookstore/screens/book_details_screen.dart';
+// import 'package:simple_bookstore/utils/constants.dart';
+//
+// import '../model/book_model.dart';
+//
+// class BookList extends StatelessWidget {
+//   const BookList({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     final List<Book> books = [
+//       Book('QUEERBOOK', 'Maia Kobabe', 20, Constants.queerBookDescription, 'images/queens.png'),
+//       Book('THE NOTE', 'Nicholas Sparks', 30, Constants.noteBookDescription, 'images/the_note.png'),
+//       Book('THE PATIENT', 'Eric Topol', 25, Constants.patientBookDescription, 'images/the_patient.png'),
+//     ];
+//
+//     return ListView.builder(
+//       itemCount: books.length,
+//       itemBuilder: (context, index) {
+//         final book = books[index];
+//         return Card(
+//           elevation: 4, // Add some elevation for a professional look
+//           margin: EdgeInsets.all(8), // Add margin for spacing
+//           child: ListTile(
+//             leading: Container(
+//               width: 80, // Set the width of the image container
+//               height: 180, // Set the height of the image container
+//               child: Image.asset(
+//                 book.image,
+//                 fit: BoxFit.cover, // Ensure the image covers the container
+//               ),
+//             ),
+//             title: Text(
+//               book.title,
+//               style: TextStyle(
+//                 fontWeight: FontWeight.bold, // Make the title bold
+//                 fontSize: 16, // Adjust the font size
+//               ),
+//             ),
+//             subtitle: Text(
+//               book.author,
+//               style: TextStyle(
+//                 fontSize: 14, // Adjust the font size
+//               ),
+//             ),
+//             trailing: Text(
+//               '\$${book.price.toStringAsFixed(2)}',
+//               style: TextStyle(
+//                 fontWeight: FontWeight.bold, // Make the price bold
+//                 fontSize: 16, // Adjust the font size
+//               ),
+//             ),
+//             onTap: () {
+//               Navigator.push(
+//                 context,
+//                 MaterialPageRoute(
+//                   builder: (context) => BookDetailsScreen(book: book),
+//                 ),
+//               );
+//             },
+//           ),
+//         );
+//       },
+//     );
+//   }
+// }
+
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:simple_bookstore/screens/book_details_screen.dart';
+// import 'package:simple_bookstore/utils/constants.dart';
+//
+// import '../model/book_model.dart';
+//
+// class BookList extends StatelessWidget {
+//   const BookList({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     final List<Book> books = [
+//       Book('QUEERBOOK', 'Maia Kobabe', 20, Constants.queerBookDescription, 'images/queens.png'),
+//       Book('THE NOTE', 'Nicholas Sparks', 30, Constants.noteBookDescription, 'images/the_note.png'),
+//       Book('THE PATIENT', 'Eric Topol', 25, Constants.patientBookDescription, 'images/the_patient.png'),
+//     ];
+//
+//     return ListView.builder(
+//       itemCount: books.length,
+//       itemBuilder: (context, index) {
+//         final book = books[index];
+//         return Padding(
+//           padding: const EdgeInsets.all(15.0),
+//           child: Card(
+//             elevation: 4, // Add some elevation for a professional look
+//             margin: EdgeInsets.all(8), // Add margin for spacing
+//             child: InkWell(
+//               onTap: () {
+//                 Navigator.push(
+//                   context,
+//                   MaterialPageRoute(
+//                     builder: (context) => BookDetailsScreen(book: book),
+//                   ),
+//                 );
+//               },
+//               child: Column(
+//                 crossAxisAlignment: CrossAxisAlignment.start,
+//                 children: [
+//                   Image.asset(
+//                     width: 400,
+//                     height: 350,
+//                     book.image,
+//
+//                     // fit: BoxFit.cover, // Ensure the image covers the container
+//                   ),
+//                   Padding(
+//                     padding: const EdgeInsets.only(left: 60, bottom: 6, top: 8),
+//                     child: Text(
+//                       book.title,
+//                       style: TextStyle(
+//                         fontWeight: FontWeight.bold, // Make the title bold
+//                         fontSize: 18, // Adjust the font size
+//                       ),
+//                     ),
+//                   ),
+//                   Padding(
+//                     padding: const EdgeInsets.only(left: 60, bottom: 6, top: 8),
+//                     child: Text(
+//                       book.author,
+//                       style: TextStyle(
+//                         fontSize: 16, // Adjust the font size
+//                       ),
+//                     ),
+//                   ),
+//                   Padding(
+//                     padding: const EdgeInsets.only(left: 60, bottom: 6, top: 8),
+//                     child: Text(
+//                       '\$${book.price.toStringAsFixed(2)}',
+//                       style: TextStyle(
+//                         fontWeight: FontWeight.bold, // Make the price bold
+//                         fontSize: 18, // Adjust the font size
+//                       ),
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ),
+//         );
+//       },
+//     );
+//   }
+// }
+
+
+
+
+
 import 'package:flutter/material.dart';
 import 'package:simple_bookstore/screens/book_details_screen.dart';
 import 'package:simple_bookstore/utils/constants.dart';
 
 import '../model/book_model.dart';
 
-class BookList extends StatelessWidget {
+class BookList extends StatefulWidget {
   const BookList({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final List<Book> books = [
-      Book('QUEERBOOK', 'Maia Kobabe', 20, Constants.queerBookDescription, 'images/queens.png'),
-      Book('THE NOTE', 'Nicholas Sparks', 30, Constants.noteBookDescription, 'images/the_note.png'),
-      Book('THE PATIENT', 'Eric Topol', 25, Constants.patientBookDescription, 'images/the_patient.png'),
-    ];
+  _BookListState createState() => _BookListState();
+}
 
+class _BookListState extends State<BookList> {
+  TextEditingController searchController = TextEditingController();
+  List<Book> books = [
+    Book('QUEERBOOK', 'Maia Kobabe', 20, Constants.queerBookDescription, 'images/queens.png'),
+    Book('THE NOTE', 'Nicholas Sparks', 30, Constants.noteBookDescription, 'images/the_note.png'),
+    Book('THE PATIENT', 'Eric Topol', 25, Constants.patientBookDescription, 'images/the_patient.png'),
+  ];
+  List<Book> filteredBooks = [];
+
+  @override
+  void initState() {
+    super.initState();
+    filteredBooks.addAll(books); // Initialize filteredBooks with all books
+  }
+
+  void filterBooks(String query) {
+    filteredBooks.clear();
+    if (query.isEmpty) {
+      filteredBooks.addAll(books); // If the query is empty, show all books
+    } else {
+      filteredBooks.addAll(books.where((book) =>
+      book.title.toLowerCase().contains(query.toLowerCase()) ||
+          book.author.toLowerCase().contains(query.toLowerCase())).toList());
+    }
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        title: Text("Bookstore"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search,color: Colors.black,size: 30,),
+            onPressed: () {
+              showSearch(context: context, delegate: BookSearch(books)); // Pass the 'books' list
+            },
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: filteredBooks.length,
+        itemBuilder: (context, index) {
+          final book = filteredBooks[index];
+          return Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Card(
+              elevation: 4,
+              margin: EdgeInsets.all(8),
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => BookDetailsScreen(book: book),
+                    ),
+                  );
+                },
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Image.asset(
+                      book.image,
+                      width: 400,
+                      height: 420,
+                      fit: BoxFit.cover,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20, bottom: 4, top: 12),
+                      child: Text(
+                        book.title,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20, bottom: 4, top: 6),
+                      child: Text(
+                        book.author,
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 20, bottom: 4, top: 6),
+                      child: Text(
+                        '\$${book.price.toStringAsFixed(2)}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class BookSearch extends SearchDelegate<Book> {
+  final List<Book> books;
+
+  BookSearch(this.books);
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        Navigator.pop(context);
+      },
+    );
+  }
+
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // Implement the search results here
+    final searchResults = books.where((book) =>
+    book.title.toLowerCase().contains(query.toLowerCase()) ||
+        book.author.toLowerCase().contains(query.toLowerCase()));
     return ListView.builder(
-      itemCount: books.length,
+      itemCount: searchResults.length,
       itemBuilder: (context, index) {
-        final book = books[index];
+        final book = searchResults.elementAt(index);
         return ListTile(
-          leading: Image.asset(book.image),
           title: Text(book.title),
           subtitle: Text(book.author),
-          trailing: Text('\$${book.price.toStringAsFixed(2)}'),
           onTap: () {
             Navigator.push(
               context,
@@ -36,4 +376,15 @@ class BookList extends StatelessWidget {
       },
     );
   }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    // You can implement suggestions here, if needed
+    return Container();
+  }
 }
+
+
+
+
+
